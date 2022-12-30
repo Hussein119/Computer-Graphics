7900148e5a1f9d421733d7b57bae00d0ba665733
7fni2003 <7fni2003@gmail.com>
2022-12-30 21:12:17 +0200

    Updated

--- Computer Graphics Code Practice/Computer Graphics Code Practice.js
+++ Computer Graphics Code Practice/Computer Graphics Code Practice.js
@@ -942,7 +942,7 @@ function animate(tr, right) {
   return newTR;
 }
 */
-// Cube  
+// Cube
 /*
 var vShader=`
 attribute vec4 myPosition;
@@ -1149,3 +1149,341 @@ function main() {
   gl.drawArrays(gl.TRIANGLE_FAN,0, n);
  }
 */
+/*
+// Clock 
+var VSHADER_SOURCE = `
+   attribute vec4 a_Position; 
+   uniform mat4 u_ModelMatrix; 
+   void main() { 
+     gl_Position = u_ModelMatrix * a_Position; 
+  }`;
+
+var FSHADER_SOURCE = `  
+   void main() {
+   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); 
+   }`;
+
+var SecsStep = -200;
+var MinsStep = SecsStep / 60.0;
+var HoursStep = MinsStep / 60.0;
+
+var HoursAngle = 0.0;
+var MinsAngle = 0.0;
+var SecsAngle = 0.0;
+
+function main() {
+  var canvas = document.getElementById("webgl");
+  var gl = getWebGLContext(canvas);
+  initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
+  var n = initVertexBuffers(gl);
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  var u_ModelMatrix = gl.getUniformLocation(gl.program, "u_ModelMatrix");
+  var modelMatrix = new Matrix4();
+
+  var tick = function () {
+    animate();
+    draw(gl, modelMatrix, u_ModelMatrix);
+    requestAnimationFrame(tick, canvas);
+  };
+  tick();
+}
+
+function initVertexBuffers(gl) {
+  var vertices = new Float32Array([
+    0.0, 0.0, 0.0, 0.2, 0.0, 0.0, 0.0, 0.4, 0.0, 0.0, 0.0, 0.6,
+  ]);
+
+  var vertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+  var a_Position = gl.getAttribLocation(gl.program, "a_Position");
+  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(a_Position);
+}
+
+function draw(gl, modelMatrix, u_ModelMatrix) {
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  modelMatrix.setRotate(HoursAngle, 0, 0, 1);
+  gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
+  gl.drawArrays(gl.LINES, 0, 2);
+
+  modelMatrix.setRotate(MinsAngle, 0, 0, 1);
+  gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
+  gl.drawArrays(gl.LINES, 2, 2);
+
+  modelMatrix.setRotate(SecsAngle, 0, 0, 1);
+  gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
+  gl.drawArrays(gl.LINES, 4, 2);
+}
+
+var g_last = Date.now();
+function animate() {
+  var now = Date.now();
+  var elapsed = now - g_last;
+  g_last = now;
+
+  HoursAngle = HoursAngle + (HoursStep * elapsed) / 1000.0;
+  MinsAngle = MinsAngle + (MinsStep * elapsed) / 1000.0;
+  SecsAngle = SecsAngle + (SecsStep * elapsed) / 1000.0;
+}
+*/
+/*
+let vshader = `
+  attribute vec4 pos;
+  uniform mat4 transformation;
+  attribute float size;
+  attribute vec4 color;
+  varying vec4 v_color;
+  void main() {
+    gl_Position = transformation * pos;
+    gl_PointSize = size;
+    v_color = color;
+  }
+`;
+
+let fshader = `
+  precision mediump float;
+  varying vec4 v_color;
+  void main() {
+    gl_FragColor = v_color;
+  }
+`;
+
+let translate = 0;
+let translate_step = 0.01;
+let angle = 0.0;
+let angle_step = 45;
+
+function main() {
+  let canvas = document.getElementById("webgl");
+  let gl = getWebGLContext(canvas);
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  initShaders(gl, vshader, fshader);
+
+  let vertices = new Float32Array([
+    0.0, 0.5, 10.0, 1.0, 0.0, 0.0, -0.5, 0.0, 20.0, 0.0, 1.0, 0.0, 0.5, 0.0,
+    30.0, 0.0, 0.0, 1.0,
+  ]);
+
+  let n = initBuffer(gl, vertices);
+
+  let frame = function () {
+    animate(gl);
+    transformation_matrix(gl);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLES, 0, n);
+    requestAnimationFrame(frame);
+  };
+
+  frame();
+}
+
+function initBuffer(gl, vertices) {
+  let vertex_buffer = gl.createBuffer();
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+
+  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+  let pos_idx = gl.getAttribLocation(gl.program, "pos");
+  let size_idx = gl.getAttribLocation(gl.program, "size");
+  let color_idx = gl.getAttribLocation(gl.program, "color");
+
+  let SIZE = vertices.BYTES_PER_ELEMENT;
+
+  gl.vertexAttribPointer(pos_idx, 2, gl.FLOAT, false, SIZE * 6, 0);
+  gl.vertexAttribPointer(size_idx, 1, gl.FLOAT, false, SIZE * 6, SIZE * 2);
+  gl.vertexAttribPointer(color_idx, 3, gl.FLOAT, false, SIZE * 6, SIZE * 3);
+
+  gl.enableVertexAttribArray(pos_idx);
+  gl.enableVertexAttribArray(size_idx);
+  gl.enableVertexAttribArray(color_idx);
+
+  return vertices.length;
+}
+
+function transformation_matrix(gl) {
+  let transformation_idx = gl.getUniformLocation(gl.program, "transformation");
+
+  let transformation = new Matrix4();
+
+  transformation.setRotate(angle, 0, 0, 1);
+  transformation.translate(translate, 0.0, 0.0);
+
+  gl.uniformMatrix4fv(transformation_idx, false, transformation.elements);
+}
+
+let last = Date.now();
+function animate() {
+  let now = Date.now();
+  let elapsed = now - last;
+  last = now;
+
+  angle = angle + (angle_step * elapsed) / 1000;
+  angle %= 360;
+
+  if (translate >= 0.5 || translate <= -0.5) translate_step *= -1;
+
+  translate += translate_step;
+}
+*/
+// // Prctice Exam 
+// var Vshader = `
+// attribute vec4 Position;
+// void main(){
+// gl_Position = Position;
+// }`;
+// var Fshader = `
+// void main(){
+//     gl_FragColor = vec4(1.0,0.0,0.0,1.0);
+// }`;
+
+// function main (){
+// var canvas = document.getElementById("webgl");
+// var gl = canvas.getContext('webgl');
+
+// if(!initShaders(gl,Vshader,Fshader)){
+//     alert("Filed pass Vshader and Fshader !!");
+//     return;
+// }
+
+// initBuffers(gl);
+
+// gl.clearColor(0,0,0,1);
+// gl.clear(gl.COLOR_BUFFER_BIT);
+// gl.drawArrays(gl.TRIANGLES, 0, 3);
+// gl.drawArrays(gl.TRIANGLE_FAN, 4, 7);
+// }
+// function initBuffers(gl){
+// var vertices = new Float32Array([
+//     -0.9,0.0,  -0.7,0.5,   -0.5,0.0, 
+//     0.0,0.0,   0.0,0.5,   0.5,0.5,  0.5,0.0
+// ]);
+// var vBuffer = gl.createBuffer();
+// gl.bindBuffer(gl.ARRAY_BUFFER,vBuffer);
+// gl.bufferData(gl.ARRAY_BUFFER, vertices,gl.STATIC_DRAW);
+// var Position = gl.getAttribLocation(gl.program,'Position');
+// gl.vertexAttribPointer(Position,2,gl.FLOAT,false,0,0);
+// gl.enableVertexAttribArray(Position);
+// }
+
+var Vshader = `
+attribute vec4 Position;
+uniform mat4 Matrix;
+void main(){
+gl_Position = Matrix * Position;
+}`;
+var Fshader = `
+void main(){
+gl_FragColor = vec4(1.0,0.0,0.0,1.0);
+}`;
+
+var ANGLE_STEP1 = 90;
+var ANGLE_STEP2 = -90;
+var ANGLE_STEP3 = -45;
+var ANGLE_STEP4 = 45;
+
+var currentAngle1 = 0.0;
+var currentAngle2 = 0.0;
+var currentAngle3 = 0.0;
+var currentAngle4 = 0.0;
+
+var transStep1 = 0.6;
+var transStep2 = 0.3;
+var transStep3 = 0.4;
+var transStep4 = 0.1;
+
+var Tx1 = 0.0 ;
+var Tx2 = 0.0 ;
+var Tx3 = 0.0 ;
+var Tx4 = 0.0 ;
+
+function main() {
+  var canvas = document.getElementById("webgl");
+  var gl = canvas.getContext("webgl");
+
+  // pass Vshader and Fshader
+  if (!initShaders(gl, Vshader, Fshader)) {
+    alert("Cannot pass Vshader and Fshader !!");
+  }
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  var Matrix = gl.getUniformLocation(gl.program, "Matrix");
+
+  var gMatrix = new Matrix4();
+
+  initBuffers(gl);
+
+  var tick = function () {
+    animate();
+    Draw(gl, gMatrix, Matrix);
+    requestAnimationFrame(tick);
+  };
+  tick();
+}
+function initBuffers(gl) {
+  var vertices = new Float32Array([
+    -0.7, 0.3, -0.43, 0.7, -0.2, 0.3,
+    0.7, 0.3, 0.2, 0.3, 0.2, 0.7, 0.7, 0.7,
+    0.7, -0.3, 0.2, -0.3, 0.2, -0.5, 0.7, -0.5,
+    -0.7, -0.4, -0.4, -0.001, -0.1, -0.4, -0.4, -0.8
+  ]);
+  var vBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+  var Position = gl.getAttribLocation(gl.program, "Position");
+  gl.vertexAttribPointer(Position, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(Position);
+}
+var last = Date.now();
+function animate() {
+  var now = Date.now();
+  var elapsed = now - last;
+  last = now;
+  currentAngle1 = currentAngle1 + (ANGLE_STEP1 * elapsed) / 1000.0;
+  currentAngle1 %= 360;
+  currentAngle2 = currentAngle2 + (ANGLE_STEP2 * elapsed) / 1000.0;
+  currentAngle2 %= 360;
+  currentAngle3 = currentAngle3 + (ANGLE_STEP3 * elapsed) / 1000.0;
+  currentAngle3 %= 360;
+  currentAngle4 = currentAngle4 + (ANGLE_STEP4 * elapsed) / 1000.0;
+  currentAngle4 %= 360;
+
+  Tx1 = Tx1 + (elapsed * transStep1) / 1000.0;
+  if (Tx1 >0.3 || Tx1 < -0.3) transStep1 *= -1;
+  Tx2 = Tx2 + (elapsed * transStep2) / 1000.0;
+  if (Tx2 >0.1 || Tx2 < -0.1) transStep2 *= -1;
+  Tx3 = Tx3 + (elapsed * transStep3) / 1000.0;
+  if (Tx3 >0.3 || Tx3 < -0.3) transStep3 *= -1;
+  Tx4 = Tx4 + (elapsed * transStep4) / 1000.0;
+  if (Tx4 >0.3 || Tx4 < -0.3) transStep4 *= -1;
+}
+function Draw(gl, gMatrix, Matrix) {
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gMatrix.setRotate(currentAngle1, 0, 0, 1);
+  gMatrix.translate(Tx1, 0, 0);
+  gl.uniformMatrix4fv(Matrix, false, gMatrix.elements);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  gMatrix.setRotate(currentAngle2, 0, 0, 1);
+  gMatrix.translate(Tx2, 0, 0);
+  gl.uniformMatrix4fv(Matrix, false, gMatrix.elements);
+  gl.drawArrays(gl.TRIANGLE_FAN, 3, 4);
+  gMatrix.setRotate(currentAngle3, 0, 0, 1);
+  gMatrix.translate(Tx3, 0, 0);
+  gl.uniformMatrix4fv(Matrix, false, gMatrix.elements);
+  gl.drawArrays(gl.TRIANGLE_FAN, 7, 4);
+  gMatrix.setRotate(currentAngle4, 0, 0, 1);
+  gMatrix.translate(Tx4, 0, 0);
+  gl.uniformMatrix4fv(Matrix, false, gMatrix.elements);
+  gl.drawArrays(gl.TRIANGLE_FAN, 11, 4);
+}
+
